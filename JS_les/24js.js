c402b115258a3f6a0d0722a6140753fdcc1216a7
VladTrokhymchuk <48185256+VladTrokhymchuk@users.noreply.github.com>
2019-03-25 23:31:01 +0200

    24-30

--- JS_les/24js.js
+++ JS_les/24js.js
@@ -54,3 +54,45 @@ let name = 'Ivan',
     });
 
     //////////////-----------27
+    function calcorDoube(number, basis = 2) {
+        // basis = basis || 2; ES5
+        console.log(number*basis);
+    }
+
+    calcorDoube(3,3);
+    calcorDoube(6);
+
+
+    class Rectangle {
+        constructor(height, width = 20) { //по умолчанию =20
+            this.height = height;
+            this.width = width;
+        }
+        calcArea() {
+            return this.height * this.width;
+        }
+    }
+
+    const square = new Rectangle(10, 10);
+    console.log(square.calcArea());
+
+
+    //оператор розворота масива
+
+    let video = ['youtube', 'vimeo', 'rutube'],
+        blogs = ['wordpress', 'bloger'],
+        intrnet = [...video, ...blogs, 'vk', 'facebook'];
+
+        console.log(intrnet);
+
+        function log(a, b, c){
+            console.log(a);
+            console.log(b);
+            console.log(c);
+            console.log(a + b + c);
+        }
+
+        let numbers = [2, 3, 6];
+        log(...numbers);
+
+        //30
