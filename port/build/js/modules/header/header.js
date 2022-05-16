d3989dd5775b14451f99e24ff6a60a8ee8283528
VladTrokhymchuk <assistantl7497@gmail.com>
2022-05-16 17:10:19 +0300

    warninig block

--- port/build/js/modules/header/header.js
+++ port/build/js/modules/header/header.js
@@ -1,34 +1,34 @@
 // borger
 function animateHamburger() {
     $('#hamburger-button').toggleClass('open');
+    /*animation from burger to X*/
+
+    // $("body").css('overflow', 'hidden');
 }
 
 function slideMenu() {
     $('.navbar').toggleClass('open');
+    /*animation for slide down menu*/
+
 }
 
 function slideLogo() {
     $('.header__logo').toggleClass('header__logo--open');
+    /*animation for slide down menu*/
 }
 
 $('#hamburger-button').click(function () {
+    // console.log('trying');
     animateHamburger();
-    slideMenu();
+    slideMenu(); /*attaching click handler to the burger button*/
     slideLogo();
 
     if (window && window.innerWidth < 1024) {
         if ($('#hamburger-button').hasClass('open')) {
             $("body").css('overflow', 'hidden');
-
         } else {
             $("body").css('overflow', 'unset');
         }
     }
 
-});
-$('.menu a').click(function () {
-    animateHamburger();
-    slideMenu();
-    slideLogo();
-    $("body").css('overflow', 'unset');
 });
